7d0d59f7a53f1e54c6d94a71d676ca6e3672bd5b
axolotlmaid <hello@axolotlmaid.com>
2024-09-14 22:04:20 +0100

    fix: add flex container to login page
    Weird global styles issue where it carries onto the next page for some
    reason

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -7,7 +7,7 @@
 	let { form }: { form: ActionData } = $props();
 
 	let show = $state(false);
-    let formElement = $state<HTMLFormElement>();
+	let formElement = $state<HTMLFormElement>();
 
 	$effect(() => {
 		show = true;
@@ -15,66 +15,77 @@
 	});
 </script>
 
-{#if show}
-	<form
-		method="POST"
-		class="widget-inner gap-3 w-[28rem] !p-8"
-        bind:this={formElement}
-        onoutroend={() => formElement?.submit()}
-		in:fly={{ duration: 1000, y: 100, easing: quintOut }}
-		out:fly={{ duration: 500, y: -100, easing: quadIn }}
-		use:enhance
-	>
-		<div id="title" class="flex justify-between items-center text-[2rem]">
-			<h1 class="text-[2.5rem] font-black">Login</h1>
-			<iconify-icon icon="mdi:cat"></iconify-icon>
-		</div>
+<svelte:head>
+	<title>login — nekopage</title>
+</svelte:head>
 
-		<div class="input">
-			<label for="username">Username</label>
-			<input
-				name="username"
-				type="text"
-				placeholder="Enter username"
-				required
-				value={form?.username ?? ''}
-			/>
-		</div>
+<div class="flex-container">
+	{#if show}
+		<form
+			method="POST"
+			class="widget-inner gap-3 w-[28rem] !p-8"
+			bind:this={formElement}
+			onoutroend={() => formElement?.submit()}
+			in:fly={{ duration: 1000, y: 100, easing: quintOut }}
+			out:fly={{ duration: 500, y: -100, easing: quadIn }}
+			use:enhance
+		>
+			<div id="title" class="flex justify-between items-center text-[2rem]">
+				<h1 class="text-[2.5rem] font-black">Login</h1>
+				<iconify-icon icon="mdi:cat"></iconify-icon>
+			</div>
 
-		<div class="input">
-			<label for="password">Password</label>
-			<input name="password" type="password" placeholder="Enter password" required />
-		</div>
+			<div class="input">
+				<label for="username">Username</label>
+				<input
+					name="username"
+					type="text"
+					placeholder="Enter username"
+					required
+					value={form?.username ?? ''}
+				/>
+			</div>
 
-		{#if form?.missing}
-			<span class="error">Missing credentials!</span>
-		{/if}
-		{#if form?.incorrect}
-			<span class="error">Incorrect!</span>
-		{/if}
+			<div class="input">
+				<label for="password">Password</label>
+				<input name="password" type="password" placeholder="Enter password" required />
+			</div>
 
-		<button type="button" onclick={() => (show = false)} class="bg-accent text-crust border-none outline-none rounded-sm w-28 h-8 mt-4 cursor-pointer transition-transform hover:scale-105 active:scale-90">Login</button>
-	</form>
+			{#if form?.missing}
+				<span class="error">Missing credentials!</span>
+			{/if}
+			{#if form?.incorrect}
+				<span class="error">Incorrect!</span>
+			{/if}
 
-    <footer in:fade out:fade class="absolute bottom-8">
-        <a href="https://github.com/axolotlmaid/nekopage" class="text-overlay">nekopage</a>
-    </footer>
-{/if}
+			<button
+				type="button"
+				onclick={() => (show = false)}
+				class="bg-accent text-crust border-none outline-none rounded-sm w-28 h-8 mt-4 cursor-pointer transition-transform hover:scale-105 active:scale-90"
+				>Login</button
+			>
+		</form>
+
+		<footer in:fade out:fade class="absolute bottom-8">
+			<a href="https://github.com/axolotlmaid/nekopage" class="text-overlay">nekopage</a>
+		</footer>
+	{/if}
+</div>
 
 <style lang="postcss">
-    :global(#theme) {
-        @apply flex justify-center items-center;
-    }
+	:global(#theme) {
+		@apply flex justify-center items-center;
+	}
 
-    input {
-        @apply w-full;
-    }
+	input {
+		@apply w-full;
+	}
 
-    .input {
-        @apply flex-col items-start;
-    }
+	.input {
+		@apply flex-col items-start;
+	}
 
-    .error {
-        @apply text-red font-medium;
-    }
+	.error {
+		@apply text-red font-medium;
+	}
 </style>
