dc284933c756d4c3d0ec006de21b2e2e8fca7039
axolotlmaid <hello@axolotlmaid.com>
2024-09-14 17:45:34 +0100

    feat: add text widget

---
+++ src/lib/widgets/Text.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	let { settings }: { settings: WidgetSettings } = $props();
+</script>
+
+<div id="text" class="widget">
+	<h1>{settings.title}</h1>
+
+	<div class="widget-inner">
+		<p>{settings.text}</p>
+	</div>
+</div>
