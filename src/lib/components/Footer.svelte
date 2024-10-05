e353fa57d137d95d55bfc27508ce14617867b1bf
trafficlunar <trafficlunar@proton.me>
2024-10-05 17:47:10 +0100

    fix: footer not importing fade transition

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+	import { fade } from "svelte/transition";
+</script>
+
 <footer in:fade out:fade class="absolute bottom-8">
     <a href="https://github.com/trafficlunar/nekopage" class="text-overlay">nekopage</a>
 </footer>
