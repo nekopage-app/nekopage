d76820cd6ea443f22debe8eadb6d83b01fd0bfdb
trafficlunar <trafficlunar@proton.me>
2024-10-05 17:47:35 +0100

    fix: remove session_id cookie in hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -27,9 +27,13 @@ export const handle: Handle = async ({ event, resolve }) => {
                 username: user.username
             };
         } else {
+            // Remove session cookie
+            event.cookies.delete("session_id", { path: "/" });
+
             // Remove session from database
             if (event.locals.user?.id)
                 database.auth.removeSession(event.locals.user?.id);
+
             // Set user to null
             event.locals.user = null;
         }
