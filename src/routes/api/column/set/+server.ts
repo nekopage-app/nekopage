ad62d00ff591ef015f87c52ad124d9ded9c72e8d
trafficlunar <trafficlunar@proton.me>
2024-10-11 15:22:07 +0100

    fix: replace `/api/widget/move` with `/api/column/set`

---
+++ src/routes/api/column/set/+server.ts
@@ -0,0 +1,21 @@
+import { json, type RequestHandler } from '@sveltejs/kit';
+
+import { Column } from '$lib/enums';
+import * as database from '$lib/server/database';
+
+export const PATCH: RequestHandler = async ({ locals, url }) => {
+	const column = url.searchParams.get('column') as Column;
+	const widgetsArrayUrlParam = url.searchParams.get('widgets');
+
+	if (!column)
+		return json({ success: false, error: 'No column was specified' }, { status: 400 });
+	if (!widgetsArrayUrlParam)
+		return json({ success: false, error: 'No widgets were specified' }, { status: 400 });
+	if (!locals.layout)
+		return json({ success: false, error: 'No layout was found' }, { status: 400 });
+	if (locals.user && !database.layouts.hasLayout(locals.user.id, locals.layout.id))
+		return json({ success: false, error: 'Unauthorized' }, { status: 401 });
+
+	database.layouts.setColumnWidgets(locals.layout.id, column, JSON.parse(widgetsArrayUrlParam));
+	return json({ success: true });
+};
