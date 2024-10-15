1bf149c7df58726e507918f9952cd12048491eed
trafficlunar <trafficlunar@proton.me>
2024-10-15 21:11:14 +0100

    fix: make RSS widget not error when unable to find elements
    also make text smaller

--- src/lib/widgets/RSS.svelte
+++ src/lib/widgets/RSS.svelte
@@ -39,26 +39,32 @@
 			class="flex flex-col bg-base rounded transition duration-200 hover:ring-2 ring-[color-mix(in_srgb,var(--accent)_30%,transparent)]"
 		>
 			{#if data.settings.showImages}
-				<img
-					src={item['media:thumbnail']['@_url']}
-					alt="rss item thumbnail"
-					class="rounded rounded-b-none object-cover h-32"
-				/>
+				{#if item['media:thumbnail']}
+					<img
+						src={item['media:thumbnail']['@_url']}
+						alt="rss item thumbnail"
+						class="rounded rounded-b-none object-cover h-32"
+					/>
+				{/if}
 			{/if}
 
 			<div id="item-information" class="p-4 h-full flex flex-col">
-				<h1 class="text-xl font-semibold">{item.title}</h1>
+				<h1 class="text-lg font-semibold">{item.title}</h1>
 
 				{#if data.settings.showDescription}
-					<hr />
-					<p>{@html DOMPurify.sanitize(item.description)}</p>
+					{#if item.description}
+						<hr />
+						<p class="text-sm">{@html DOMPurify.sanitize(item.description)}</p>
+					{/if}
 				{/if}
 
 				{#if data.settings.showPublished}
-					<div id="published" class="mt-auto">
-						<hr class="mb-1" />
-						<sub class="text-overlay">{item.pubDate}</sub>
-					</div>
+					{#if item.pubDate}
+						<div id="published" class="mt-auto">
+							<hr class="mb-1" />
+							<sub class="text-overlay">{item.pubDate}</sub>
+						</div>
+					{/if}
 				{/if}
 			</div>
 		</a>
