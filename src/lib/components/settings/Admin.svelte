765d4c9fb3442f4a7e67649b23b49e740ebf9ceb
trafficlunar <trafficlunar@proton.me>
2024-11-01 22:34:05 +0000

    feat: add user creating in admin tab

--- src/lib/components/settings/Admin.svelte
+++ src/lib/components/settings/Admin.svelte
@@ -1,61 +1,107 @@
 <script lang="ts">
-    import { onMount } from "svelte";
+	import { onMount } from 'svelte';
 
-	import { page } from "$app/stores";
-	import { UserPermission } from "$lib/enums";
+	import { page } from '$app/stores';
+	import { UserPermission } from '$lib/enums';
+    import { errorMessage, showErrorMessage } from '$lib/stores';
 
-    let users = $state([]);
+	import Modal from '../Modal.svelte';
+	import PasswordInput from '../inputs/PasswordInput.svelte';
 
-    onMount(async () => {
-        const response = await fetch("/api/admin/users");
+	let users = $state([]);
+
+	onMount(async () => {
+		const response = await fetch('/api/admin/users');
+		const data = await response.json();
+
+		users = data.users;
+	});
+
+	let showCreateUserModal = $state(false);
+
+    let createUserUsername = $state("");
+    let createUserPassword = $state("");
+
+	async function createUser() {
+		showCreateUserModal = false;
+
+        const response = await fetch("/api/admin/users", { method: "POST", body: JSON.stringify({ username: createUserUsername, password: createUserPassword }) });
         const data = await response.json();
 
-        users = data.users;
-    });
+        if (!data.success) {
+            showErrorMessage.set(true);
+            errorMessage.set(data.error);
+        }
+	}
 </script>
 
 {#if $page.data.permissions.includes(UserPermission.Administrator)}
-    <button class="button">Create User</button>
+	<button class="button" onclick={() => (showCreateUserModal = true)}>Create User</button>
 
-    <hr>
+	<hr />
 
-    <table class="w-full">
-        <thead class="bg-base">
-            <tr>
-                <th>ID</th>
-                <th>Username</th>
-                <th class="w-40">Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            {#each users as { id, username }}
-                <tr>
-                    <td>{id}</td>
-                    <td>{username}</td>
-                    <td>
-                        <button class="button">Edit</button>
-                        <button class="button !bg-red">Delete</button>
-                    </td>
-                </tr>
-            {/each}
-        </tbody>
-    </table>
+	<table class="w-full">
+		<thead class="bg-base">
+			<tr>
+				<th>ID</th>
+				<th>Username</th>
+				<th class="w-40">Actions</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each users as { id, username }}
+				<tr>
+					<td>{id}</td>
+					<td>{username}</td>
+					<td>
+						<button class="button">Edit</button>
+						<button class="button !bg-red">Delete</button>
+					</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
 {:else}
-    <!-- This shouldn't happen but we shouldn't risk it either -->
-    <div class="flex-container flex-col backdrop-blur backdrop-brightness-75">
-        <iconify-icon icon="ion:warning" class="text-6xl"></iconify-icon>
-        <p class="text-2xl">You're not an administrator!</p>
+	<!-- This shouldn't happen but we shouldn't risk it either -->
+	<div class="flex-container flex-col backdrop-blur backdrop-brightness-75">
+		<iconify-icon icon="ion:warning" class="text-6xl"></iconify-icon>
+		<p class="text-2xl">You're not an administrator!</p>
 
-        <p class="absolute bottom-8 text-overlay">refresh to exit screen</p>
-    </div>
+		<p class="absolute bottom-8 text-overlay">refresh to exit screen</p>
+	</div>
 {/if}
 
-<style lang="postcss">
-    th {
-        @apply font-semibold text-left;
-    }
+<Modal
+	id="create-user-modal"
+	title="create user"
+	bind:show={showCreateUserModal}
+	class="gap-2 !h-60"
+>
+	<div class="input input-vertical">
+		<label for="username">Username</label>
+		<input name="username" type="text" placeholder="Enter username" bind:value={createUserUsername} />
+	</div>
 
-    th, td {
-        @apply p-1 px-2 border border-surface;
-    }
+	<div class="input input-vertical">
+		<label for="password">Password</label>
+		<PasswordInput placeholder="Enter password" bind:value={createUserPassword} />
+	</div>
+
+	<hr />
+
+	<div class="grid grid-cols-2 gap-2 mt-auto">
+		<button class="button" onclick={() => (showCreateUserModal = false)}>Cancel</button>
+		<button class="button" onclick={createUser}>Create</button>
+	</div>
+</Modal>
+
+<style lang="postcss">
+	th {
+		@apply font-semibold text-left;
+	}
+
+	th,
+	td {
+		@apply p-1 px-2 border border-surface;
+	}
 </style>
--- src/routes/api/admin/users/+server.ts
+++ src/routes/api/admin/users/+server.ts
@@ -3,6 +3,11 @@ import { json, type RequestHandler } from '@sveltejs/kit';
 import { UserPermission } from '$lib/enums';
 import * as database from '$lib/server/database';
 
+// Minimum three characters, maximum 32 characters, allows only letters, digits, and underscores
+const usernameFilter = /^[a-zA-Z0-9_]{3,32}$/;
+// Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and, one special character
+const passwordFilter = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
 export const GET: RequestHandler = async ({ locals }) => {
 	if (
 		!locals.user ||
@@ -13,3 +18,21 @@ export const GET: RequestHandler = async ({ locals }) => {
 
 	return json({ success: true, users: database.auth.getUsers() });
 };
+
+export const POST: RequestHandler = async ({ locals, request }) => {
+	const { username, password } = await request.json();
+
+	if (!username || !password)
+		return json({ success: false, error: "Username or password missing!" }, { status: 400 });
+	if (!usernameFilter.test(username) || !passwordFilter.test(password))
+		return json({ success: false, error: "Username or password filtered!" }, { status: 400 });
+	if (
+		!locals.user ||
+		(locals.user &&
+			!database.permissions.hasPermission(locals.user.id, UserPermission.Administrator))
+	)
+		return json({ success: false, error: 'Unauthorized' }, { status: 401 });
+
+	database.auth.createUser(username, password);
+	return json({ success: true });
+}
